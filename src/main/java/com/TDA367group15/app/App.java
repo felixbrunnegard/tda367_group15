80ad093678d115ed5cdcc3b0548eaad848af012b
felixbrunnegard <44345418+felixbrunnegard@users.noreply.github.com>
2022-08-30 11:54:13 +0200

    Maven project added

---
+++ src/main/java/com/TDA367group15/app/App.java
@@ -0,0 +1,13 @@
+package com.TDA367group15.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
