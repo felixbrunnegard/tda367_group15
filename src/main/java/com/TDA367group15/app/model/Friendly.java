dec9f6724dcb689241d95b7b854e6ad72068c8ba
Susu <yanliw@net.chalmers.se>
2022-09-22 11:11:49 +0200

    skeleton till NPC

---
+++ src/main/java/com/TDA367group15/app/model/Friendly.java
@@ -0,0 +1,8 @@
+package com.TDA367group15.app.model;
+
+public class Friendly {
+    private boolean interactable;
+
+    public void interact(Player p){
+    }
+}
---
+++ src/main/java/com/TDA367group15/app/model/NurseNPC.java
@@ -0,0 +1,7 @@
+package com.TDA367group15.app.model;
+
+public class NurseNPC extends Friendly{
+
+    public void givePlayerHP(){
+    }
+}
