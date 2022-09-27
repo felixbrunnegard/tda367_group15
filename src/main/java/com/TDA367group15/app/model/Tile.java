35e1c5c3341772aca09938391a2a6f9cead986d3
itggot-gustaf-algeskog <35598573+itggot-gustaf-algeskog@users.noreply.github.com>
2022-09-27 11:53:32 +0200

    Implemented method to draw tiles from csv file

--- src/main/java/com/TDA367group15/app/model/Tile.java
+++ src/main/java/com/TDA367group15/app/model/Tile.java
@@ -8,17 +8,19 @@ public class Tile {
     public Tile(int tileID){
 
         switch (tileID){
-            case 1: this.fileName = "earth.png";
-                    break;
-            case 2: this.fileName = "grass.png";
+            case 1: this.fileName = "grass.png";
+                break;
+            case 2: this.fileName = "water.png";
                 break;
             case 3: this.fileName = "sand.png";
                 break;
-            case 4: this.fileName = "tree.png";
+            case 4: this.fileName = "wall.png";
                 break;
             case 5: this.fileName = "wall.png";
                 break;
-            case 6: this.fileName = "water.png";
+            case 6: this.fileName = "sand.png";
+                break;
+            case 7: this.fileName = "tree.png";
                 break;
             default: this.fileName = null;
                 break;
