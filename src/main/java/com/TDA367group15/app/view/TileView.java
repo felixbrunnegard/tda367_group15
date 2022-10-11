fc6c8043698ccfc4e8b7e6ffc69830b2435fef50
itggot-gustaf-algeskog <35598573+itggot-gustaf-algeskog@users.noreply.github.com>
2022-10-11 10:36:36 +0200

    Changed enemy sprite now rendering on a fixed position

--- src/main/java/com/TDA367group15/app/view/TileView.java
+++ src/main/java/com/TDA367group15/app/view/TileView.java
@@ -100,9 +100,6 @@ public class TileView extends WorldView {
                 x - tileSize < player.getPosition().getX() + GameView.SCREEN_WIDTH/2 &&
                 y + tileSize > player.getPosition().getY() - GameView.SCREEN_ROW/2 &&
                 y - tileSize < player.getPosition().getY() + GameView.SCREEN_ROW/2) {
-                if (tileNum == 0){
-                    System.out.println(row);
-                }
                 g2.drawImage(images.get(tileNum-1), screenX, screenY, tileSize, tileSize, null);
             }
 
