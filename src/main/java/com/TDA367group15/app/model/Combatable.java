a55f56ea6e6b3cb882b6cb9e263ea62a09e00bed
Susu <yanliw@net.chalmers.se>
2022-10-13 09:21:14 +0200

    deleted commented code

--- src/main/java/com/TDA367group15/app/model/Combatable.java
+++ src/main/java/com/TDA367group15/app/model/Combatable.java
@@ -3,9 +3,7 @@ package com.TDA367group15.app.model;
 public abstract class Combatable extends Entity {
     private int health;
     private int level;
-    //private List<Ability> abilities;
     public Combatable(int x, int y) {
         super(x, y);
     }
-
 }
