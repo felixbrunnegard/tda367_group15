5111352dd0ac08cba7a1070520166dcb190d3d96
Susu <yanliw@net.chalmers.se>
2022-10-17 11:18:21 +0200

    specified import

--- src/test/java/com/TDA367group15/app/PlayerTest.java
+++ src/test/java/com/TDA367group15/app/PlayerTest.java
@@ -5,7 +5,9 @@ import com.TDA367group15.app.model.Enemy;
 import com.TDA367group15.app.model.Player;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class PlayerTest {
     @Test
