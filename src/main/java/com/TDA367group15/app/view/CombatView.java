97ef77dc51d5d6b8a8fb90f658f53fc4b25f7d3b
itggot-gustaf-algeskog <35598573+itggot-gustaf-algeskog@users.noreply.github.com>
2022-10-18 15:41:36 +0200

    Added placeholder combat image and made a draw method in combatView

--- src/main/java/com/TDA367group15/app/view/CombatView.java
+++ src/main/java/com/TDA367group15/app/view/CombatView.java
@@ -1,8 +1,27 @@
 package com.TDA367group15.app.view;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class CombatView {
 
-    public CombatView() {
+    private BufferedImage image;
+    private int width;
+    private int height;
 
+    public CombatView(int width, int height) {
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/combat.png"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        this.width = width;
+        this.height = height;
+    }
+
+    public void draw(Graphics2D g2){
+        g2.drawImage(image, 0, 0, width, height, null);
     }
 }
